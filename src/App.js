bc6fa056e54c46a49152e01ebc4750282e80dc68
focusgabriel <charlesuchendu750@gmail.com>
2026-03-30 15:44:17 +0100

    react checkpoint

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { Container } from "react-bootstrap";
+
+function Navapp() {
+    return (
+        <Nav fill variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+            Disabled
+            </Nav.Link>
+        </Nav.Item>
+        </Nav>
+    );
+}
+
+
+
+function PageHeader() {
+  return (
+    <Container className="text-center my-5">
+      <h1 className="display-3">Welcome to My Page</h1>
+      <p className="lead">Ever, Welcome to My Page</p>
+    </Container>
+  );
+}
+
+function GridExample() {
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+function App() {
+    return(
+        <>
+            <Navapp />
+            <PageHeader />
+            <GridExample />
+        </>
+    );
+}
+
+export default App;
